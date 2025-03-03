5562085c890f7c781ccbe08f0873213c3f6ba773
Gali2025 <magali.mougey@gmail.com>
2025-03-03 23:26:58 +0000

    update comment in importation csv

--- sql/qof_indices_import.py
+++ sql/qof_indices_import.py
@@ -1,6 +1,12 @@
+#creer la df
 import pandas as pd
 
 file = "quality_of_life_indices.csv"
 
 df = pd.read_csv(file)
-print(df.tail(10))
+print(df.tail())
+
+#voir les donnees
+print(df.info())
+print(df.describe(include = "all"))
+#next : nettoyer au besoin et transferer via DB API connect & cursor()
